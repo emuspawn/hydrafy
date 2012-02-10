13cdf86ef02890d79cf9a55ff6f889c76d4c8da1
configitnow@gmail.com <configitnow@gmail.com@7bf54eb4-928e-380f-bfed-8b8e7a9314d2>
2012-02-10 03:27:00 +0000

    See changes.txt

--- trunk/hydrafy.sh
+++ trunk/hydrafy.sh
@@ -5,18 +5,18 @@ function script_info--()
 ## Filename: hydrafy.sh
 ## Copyright (C) <2012>  <Snafu>
 
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software Foundation, either version 3 of the License, or
-##  (at your option) any later version.
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
 
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License for more details.
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
 
-##  You should have received a copy of the GNU General Public License
-##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
 
@@ -56,6 +56,7 @@ function script_info--()
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~ Planned Implementations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+## Creation of a function that will save username and passwords to seperate files if desired
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
 
@@ -70,9 +71,11 @@ function script_info--()
 ## Support for the Router Attack method is limited to browser-based http-get requests right now,
 ## Feel free to do the work for me, if your work is good....I'll implement it and give you credit
 
-## A patched version of hydra has been included with the svn repo of hydrafy.  I included the original hydra.c file renamed to orig.hydra.c.orig for comparisons.  To compile it do: tar xfzv hydra-7.1-src.tar.gz and drop into the directory, read the README and go from there.
+## Version 7.2 of hydra can be obtained via: wget http://www.thc.org/releases/hydra-7.2-src.tar.gz
 
 ## There is now an option for -C -or- -L and -P with reference to methodology of attack
+
+## router.lst has been parsed for duplicate values and I have removed as many of them as I could find.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~## 
 
 
@@ -87,11 +90,14 @@ function script_info--()
 
 ## @The_Eccentric for reigniting the spark to publish this script
 
+## TAPE for making me look deper into regex and pointing out the 360+ duplicate usernames and passwords within router.lst
+
 ## Kudos to my wife for always standing by my side, having faith in me, and showing the greatest of patience for my obsession with hacking.
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 sleep 0
 }
 
+##~~~~~~~~~~~~~~~~~~~~~~~~ BEGIN Repitious Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~##
 ## Usage funtion for printing the help message
 function usage--()
 {
@@ -107,21 +113,48 @@ Usage:
 "
 }
 
-function menu--()
+parser--()
 {
-selection= ## Menu Choice
+#p_value= ## Parsing style
 clear
 echo -e "\033[1;34m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -------------------------------------------------------
-                  Make Your Selection
+                     Parser Style
 -------------------------------------------------------
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[36m
-1 ~~~~~~> Display Usernames:Passwords
+1) Anywhere\033[1;32m                 [grep -i <brand of router>]\033[36m
 
-2 ~~~~~~> Save Usernames:Passwords to a file
+2) Starts with\033[1;32m             [grep -i ^<brand of router>]\033[36m
 
-3 ~~~~~~> Implement Router Attack via Hydra\033[1;34m
+3) Word match\033[1;32m             [grep -iwx <brand of router>]\033[1;34m
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+read p_value
+case $p_value in
+	1|2|3) menu--;;
+	*) echo -e "\033[31m\nYOU MUST MAKE A VALID SELECTION TO PROCEED"
+	sleep 1
+	parser--;;
+esac
+}
+##~~~~~~~~~~~~~~~~~~~~~~~~~ END Repitious Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+
+
+function menu--()
+{
+#selection= ## Menu Choice
+clear
+echo -e "\033[1;34m
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-------------------------------------------------------
+                   Hydrafy Choices
+-------------------------------------------------------
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[36m
+1) Display Usernames:Passwords
+
+2) Save Usernames:Passwords to a file
+
+3) Implement Router Attack via Hydra\033[1;34m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 read selection
 case $selection in
@@ -131,7 +164,7 @@ case $selection in
 
 	3) hydrafy--;;
 
-	*) echo -e "\033[31m\nYOU MUST MAKE A SELECTION TO PROCEED"
+	*) echo -e "\033[31m\nYOU MUST MAKE A VALID SELECTION TO PROCEED"
 	sleep 1
 	menu--;;
 esac
@@ -140,12 +173,19 @@ esac
 function display--()
 {
 clear
-echo -e "\033[1;33m
---------------------------------------------------------"
-grep -i $ROUTER $FILE | awk -F\| '{ print $2":"$3 }'
-echo "--------------------------------------------------------
+echo -e "\033[1;34m
+--------------------------------------------------------\033[1;33m"
+case $p_value in
+	1) grep -i $ROUTER $FILE | awk -F\| '{ print $2":"$3 }';;
 
-The names above were derived from this series of routers: $ROUTER
+	2) grep -i ^$ROUTER $FILE | awk -F\| '{ print $2":"$3 }';;
+
+	3) grep -iwx $ROUTER $FILE | awk -F\| '{ print $2":"$3 }';;
+esac
+
+echo -e "\033[1;34m--------------------------------------------------------\033[1;32m
+
+The usernames/passwords above were derived from this wordsearch:\033[1;33m $ROUTER
 "
 }
 
@@ -158,11 +198,18 @@ while [ -z $SAVE ];do
 	read SAVE
 done
 
-grep -i $ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > $SAVE
-echo -e "\033[1;33m
-File Written to: $SAVE
+case $p_value in
+	1) grep -i $ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > $SAVE;;
 
-File data derived from this series of routers: $ROUTER
+	2) grep -i ^$ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > $SAVE;;
+
+	3) grep -iwx $ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > $SAVE;;
+esac
+
+echo -e "\033[1;32m
+File Written to:\033[1;33m $SAVE\033[1;32m
+
+The usernames/passwords above were derived from this wordsearch:\033[1;33m $ROUTER
 "
 }
 
@@ -171,40 +218,60 @@ function hydrafy--()
 style= ## Method of attack
 ip= ## Tgt address for hydra
 at= ## path for past root
-clear
 while [ -z $style ];do
-	echo -e "\033[1;34m\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Username/Password Attack Style
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[1;36m
-1) -C (recommended)
-2) -L and -P\033[1;34m
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+	clear
+	echo -e "\033[1;34m
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-------------------------------------------------------------------------------------------
+                               Username/Password Attack Style
+-------------------------------------------------------------------------------------------
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[36m
+1) -C (recommended)\033[1;32m    [colon separated "login:pass" format]\033[31m (Recommended)\033[36m
+
+2) -L and -P\033[1;32m           [load logins from $FILE and load passwords from $FILE]\033[1;34m
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	read style
 	case $style in
-		1) grep -i $ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > snakebite.txt ;;
+		1) case $p_value in
+			1) grep -i $ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > snakebite.txt;;
 
-		2) grep -i $ROUTER $FILE | awk -F\| '{ print $2 }' | sort | uniq > user.txt
-		grep -i $ROUTER $FILE | awk -F\| '{ print $3 }' | sort | uniq > pass.txt ;;
+			2) grep -i ^$ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > snakebite.txt;;
 
-		*) style= ;; ## Nulled
+			3) grep -iwx $ROUTER $FILE | awk -F\| '{ print $2":"$3 }' > snakebite.txt;;
+		esac;;
+
+		2) case $p_value in
+			1) grep -i $ROUTER $FILE | awk -F\| '{ print $2 }' | sort | uniq > user.txt
+			grep -i $ROUTER $FILE | awk -F\| '{ print $3 }' | sort | uniq > pass.txt;;
+
+			2) grep -i ^$ROUTER $FILE | awk -F\| '{ print $2 }' | sort | uniq > user.txt
+			grep -i ^$ROUTER $FILE | awk -F\| '{ print $3 }' | sort | uniq > pass.txt;;
+
+			3) grep -iwx $ROUTER $FILE | awk -F\| '{ print $2 }' | sort | uniq > user.txt
+			grep -iwx $ROUTER $FILE | awk -F\| '{ print $3 }' | sort | uniq > pass.txt;;
+		esac;;
+
+		*) echo -e "\033[31m\nYOU MUST MAKE A SELECTION TO PROCEED"
+		sleep 1 
+		style= ;; ## Nulled
 	esac
 
 done
 
 clear
 echo -e "\033[1;36m\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[1;34m
-########Example URL#########
+########Example URL#########\033[1;32m
 http://example.com/index.asp
 -or-
-http://192.168.1.1/index.asp
-############################
+http://192.168.1.1/index.asp\033[1;34m
+############################\033[1;32m
 
 index.asp will describe the \"path past root\"
 \033[1;36m
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
 while [ -z $ip ];do
 	echo -e "\033[36m
-Tgt IP/Hostname? -----> {http:// is assumed by default} <-----"
+Tgt IP/Hostname? ----->\033[1;32m {http:// is prepended by default}\033[1;34m <-----"
 	read ip
 done
 
@@ -249,10 +316,10 @@ while getopts "f:r:" options; do
   esac
 done
 
-current_ver="0.8"
-rel_date="5 February 2012"
+current_ver="1.0"
+rel_date="9 February 2012"
 if [[ -n "$FILE" && -n "$ROUTER" ]]; then
-	menu--
+	parser--
 else
 	usage--
 fi
